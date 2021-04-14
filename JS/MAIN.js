0f92c3cebf07be7eaca5135493cf4294667fd5b6
MohZaib-Ali <68590303+MohZaib-Ali@users.noreply.github.com>
2021-04-14 21:14:26 +0500

    Update Finalles

--- JS/MAIN.js
+++ JS/MAIN.js
@@ -4,8 +4,8 @@ var Connections = 0;
 var Messages = 0;
 
 // Fetching
-const Connect = () => {
-  let ws = new WebSocket("wss://gold-rates-usd.herokuapp.com");
+const Connect = async () => {
+  let ws = await new WebSocket("ws://localhost:5000"); //"wss://gold-rates-usd.herokuapp.com");
   var z = `6F92AD7721C33520FC815BCA7E8BB297B96C21B17CDB03872F71`;
   var y = "Developed_By_MohZaib_Tech";
   ws.addEventListener("open", () => {
@@ -16,7 +16,7 @@ const Connect = () => {
       })
     );
   });
-  ws.addEventListener("message", (e) => {
+  ws.addEventListener("message", async (e) => {
     if (JSON.parse(e.data).Error == undefined) {
       Messages++;
       Data_Manipulate(JSON.parse(e.data));
@@ -26,12 +26,15 @@ const Connect = () => {
             Type: "Subscribe",
           })
         );
-      }, 1000);
+      }, 500);
     } else {
       ws.close();
     }
   });
+  ws.addEventListener("close", () => {});
+  Data_Manipulate(false);
 };
+
 // Manipulation
 const DF = (N, O) => {
   N = parseFloat(N).toFixed(2);
@@ -47,8 +50,8 @@ const DF = (N, O) => {
     }
   }
   FD = FD[0];
-  const SS = NS.substring(0, FD);
-  const DS = NS.substring(FD);
+  const SS = Format(NS.substring(0, FD));
+  const DS = Format(NS.substring(FD));
   if (DIFF < 0) {
     return `${SS}<span class="DOWN">${DS}</span><span class="DOWN_IMG"><img src="IMG/SORT_DOWN.svg"/></span>`;
   } else if (DIFF > 0) {
@@ -57,23 +60,35 @@ const DF = (N, O) => {
     return `${SS}`;
   }
 };
+const Format = (x) => {
+  x = x.toString();
+  var p = /(-?\d+)(\d{3})/;
+  while (p.test(x)) x = x.replace(p, "$1,$2");
+  return x;
+};
 const Data_Manipulate = (JD) => {
   const JS = ["OZ", "GM", "KG", "TT"];
   const ARR = ["BP", "AP", "HP", "LP"];
-
-  if (typeof Storage !== "undefined") {
+  if (JD == false) {
+    for (var j = 0; j < JS.length; j++) {
+      for (var i = 0; i < ARR.length; i++) {
+        var Elem = document.querySelector(`#${ARR[i]}${JS[j]}`);
+        Elem.innerHTML = "-";
+      }
+    }
+  } else if (typeof Storage !== "undefined") {
     if (sessionStorage.getItem(Site) == null) {
       for (var j = 0; j < JS.length; j++) {
         for (var i = 0; i < ARR.length; i++) {
           var Elem = document.querySelector(`#${ARR[i]}${JS[j]}`);
-          Elem.innerHTML = JD[JS[j]][ARR[i]];
+          Elem.innerHTML = Format(JD[JS[j]][ARR[i]]);
         }
       }
     } else if (JSON.parse(sessionStorage.getItem(Site)).length != JD.length) {
       for (var j = 0; j < JS.length; j++) {
         for (var i = 0; i < ARR.length; i++) {
           var Elem = document.querySelector(`#${ARR[i]}${JS[j]}`);
-          Elem.innerHTML = JD[JS[j]][ARR[i]];
+          Elem.innerHTML = Format(JD[JS[j]][ARR[i]]);
         }
       }
     } else if (JSON.parse(sessionStorage.getItem(Site)) == JD) {
@@ -81,7 +96,7 @@ const Data_Manipulate = (JD) => {
       for (var j = 0; j < JS.length; j++) {
         for (var i = 0; i < ARR.length; i++) {
           var Elem = document.querySelector(`#${ARR[i]}${JS[j]}`);
-          Elem.innerHTML = TD[JS[j]][ARR[i]];
+          Elem.innerHTML = Format(TD[JS[j]][ARR[i]]);
         }
       }
     } else {
@@ -99,7 +114,7 @@ const Data_Manipulate = (JD) => {
       for (var j = 0; j < JS.length; j++) {
         for (var i = 0; i < ARR.length; i++) {
           var Elem = document.querySelector(`#${ARR[i]}${JS[j]}`);
-          Elem.innerHTML = JD[JS[j]][ARR[i]];
+          Elem.innerHTML = Format(JD[JS[j]][ARR[i]]);
         }
       }
     } else if (
@@ -108,7 +123,7 @@ const Data_Manipulate = (JD) => {
       for (var j = 0; j < JS.length; j++) {
         for (var i = 0; i < ARR.length; i++) {
           var Elem = document.querySelector(`#${ARR[i]}${JS[j]}`);
-          Elem.innerHTML = JD[JS[j]][ARR[i]];
+          Elem.innerHTML = Format(JD[JS[j]][ARR[i]]);
         }
       }
     } else if (JSON.parse(document.cookie.substring(Site.length + 1)) == JD) {
@@ -116,7 +131,7 @@ const Data_Manipulate = (JD) => {
       for (var j = 0; j < JS.length; j++) {
         for (var i = 0; i < ARR.length; i++) {
           var Elem = document.querySelector(`#${ARR[i]}${JS[j]}`);
-          Elem.innerHTML = TD[JS[j]][ARR[i]];
+          Elem.innerHTML = Format(TD[JS[j]][ARR[i]]);
         }
       }
     } else {
